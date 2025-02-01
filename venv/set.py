c95d6b941cfc539c07e7076b48025318d2a4dee5
Sambhavi Sharma <149924866+Sambhavi374@users.noreply.github.com>
2025-02-01 06:20:00 +0000

    setup

---
+++ venv/set.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages ,setup 
+
+
+setup(
+name="Mlprojects",
+version='0.0.1',
+author='Sambhavi',
+author_email='sambhavi374@gmail.com',
+package=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+
+
+)
