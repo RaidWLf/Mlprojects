b8349a7257aa7a454d38483439da6ee07156e199
Sambhavi Sharma <149924866+Sambhavi374@users.noreply.github.com>
2025-02-01 06:37:29 +0000

    requirements

--- venv/set.py
+++ venv/set.py
@@ -1,4 +1,17 @@
 from setuptools import find_packages ,setup 
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements        
 
 
 setup(
@@ -7,7 +20,8 @@ version='0.0.1',
 author='Sambhavi',
 author_email='sambhavi374@gmail.com',
 package=find_packages(),
-install_requires=['pandas','numpy','seaborn']
+install_requires= get_requirements('requirement.txt')
+
 
 
 )
