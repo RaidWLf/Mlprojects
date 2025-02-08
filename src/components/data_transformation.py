431ea99c690213d8e018f581f9287f93f304fa2b
Sambhavi Sharma <149924866+Sambhavi374@users.noreply.github.com>
2025-02-08 11:15:17 +0000

    Datatransformation config

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -20,8 +20,42 @@ class DataTransformation:
         self.data_transformation_config = DataTransformationConfig()
     
     def get_data_tranformer_object(self):
+
+    # This function is responsble for the data transformation.
+
         try:
             numerical_columns = ['writing score','reading score']
+            categorical_columns = ['gender','race_ethinicity','parental level of education','lunch','test preparation course']
+            num_pipeline = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy='median')),
+                    ('std_scaler', StandardScaler())
+                ]
+            )
+            cat_pipeline = Pipeline(
+                steps=[
+                    ('imputer', SimpleImputer(strategy='most_frequent')),
+                    ('onehot', OneHotEncoder()),
+                    ('scaling', StandardScaler())
+                ]
+            )
             
-        except:
-            pass    
+            logging.info("numerical and categorical pipeline created successfully.")
+
+            full_pipeline = ColumnTransformer(
+                transformers=[
+                    ('num', num_pipeline, numerical_columns),
+                    ('cat', cat_pipeline, categorical_columns)
+                ]
+            )
+
+            logging.info("Data Transformation object created successfully")
+
+            return full_pipeline
+
+
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+    
