c753e76c51c872be2ac69ad3b02acfc6f80cff69
Sambhavi Sharma <149924866+Sambhavi374@users.noreply.github.com>
2025-02-14 16:34:48 +0000

    errors solved -2 unsolved -1

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -10,9 +10,9 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            
-            model_path = 'artifacts\model.pkl'
-            preprocessor_path = 'artifacts\preprocessor.pkl'
+
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path = 'artifacts/preprocessor.pkl'
             model = load_object(file_path = model_path)
             preprocessor = load_object(file_path = preprocessor_path)
             data_scaled = preprocessor.transform(features)
@@ -41,6 +41,7 @@ class CustomData:
         self.lunch = lunch
         self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
+        self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
         try:
